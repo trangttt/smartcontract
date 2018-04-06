ea66e9ff1a86d56d91fb15b9e5c2598033fd608c
trungtt <trungtt1981@gmail.com>
2018-04-06 21:33:24 +0700

    Refactor and implement functions of transferring seed and presale tokens

--- ShareToken.sol
+++ ShareToken.sol
@@ -71,12 +71,12 @@ contract ERC20Interface {
     function allowance(address _owner, address _spender) constant returns (uint remaining);
 }
 
-contract ERC20Token is ERC20Interface, Owned {
+contract ERC20Token is ERC20Interface {
 
     using SafeMath for uint;
 
     // Total amount of tokens issued
-    uint totalTokenIssued;
+    uint internal totalTokenIssued;
 
     mapping(address => uint) balances;
     mapping(address => mapping (address => uint)) allowed;
@@ -170,12 +170,193 @@ contract WhitelistedCrowdsale is Owned {
     }
 }
 
-contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
+contract ShareToken is ERC20Token, Owned {
 
-    string public constant name     = "ShareToken";
-    string public constant symbol   = "SHR";
+    using SafeMath for uint;
+
+    string public constant name = "ShareToken";
+    string public constant symbol = "SHR";
     uint8  public constant decimals = 2;
-    uint constant E2 = 10**2; // any token amount must be multiplied by this const to reflect decimals
+
+    // Any token amount must be multiplied by this const to reflect decimals
+    uint constant E2 = 10**2;
+
+    mapping(address => bool) locked;
+
+    uint public constant TOKEN_SUPPLY_AIRDROP_LIMIT  = 6666666667; // 66,666,666.67 tokens (0.066 billion)
+    uint public constant TOKEN_SUPPLY_BOUNTY_LIMIT   = 33333333333; // 333,333,333.33 tokens (0.333 billion)
+
+    uint public airDropTokenIssuedTotal;
+    uint public bountyTokenIssuedTotal;
+
+    uint public constant TOKEN_SUPPLY_SEED_LIMIT      = 500000000 * E2; // 500,000,000 tokens (0.5 billion)
+    uint public constant TOKEN_SUPPLY_PRESALE_LIMIT   = 2500000000 * E2; // 2,500,000,000.00 tokens (2.5 billion)
+
+    uint public seedTokenIssuedTotal;
+    uint public presaleTokenIssuedTotal;
+
+    function ShareToken() public {
+
+        totalTokenIssued = 0;
+        airDropTokenIssuedTotal = 0;
+        bountyTokenIssuedTotal = 0;
+        seedTokenIssuedTotal = 0;
+        presaleTokenIssuedTotal = 0;
+    }
+
+    function transfer(address _to, uint _amount) returns (bool success) {
+
+        require( locked[msg.sender] == false );    
+        require( locked[_to] == false );
+        
+        return super.transfer(_to, _amount);
+    }
+
+    function transferFrom(address _from, address _to, uint _amount) returns (bool success) {
+        
+        require( locked[_from] == false );
+        require( locked[_to] == false );
+
+        return super.transferFrom(_from, _to, _amount);
+    }
+
+    function unLock(address _participant) onlyOwner {
+
+        locked[_participant] = false;
+    }
+
+    function unLockMultiple(address[] _participants) onlyOwner {
+
+        for (uint i = 0; i < _participants.length; i++) {
+            locked[_participants[i]] = false;
+        }
+    }
+
+    function sell(address buyer, uint tokens) onlyOwner returns (bool success) {
+      
+        require (tokens > 0);
+
+        // Register tokens issued to the buyer
+        balances[buyer] = balances[buyer].add(tokens);
+
+        // Update total amount of tokens issued
+        totalTokenIssued = totalTokenIssued.add(tokens);
+
+        // Lock the buyer
+        locked[msg.sender] = true;
+
+        Transfer(address(0x0), buyer, tokens);
+
+        return true;
+    }
+
+    function rewardAirdrop(address _to, uint _amount) onlyOwner {
+
+        require(airDropTokenIssuedTotal < TOKEN_SUPPLY_AIRDROP_LIMIT);
+
+        uint remainingTokens = TOKEN_SUPPLY_AIRDROP_LIMIT.sub(airDropTokenIssuedTotal);
+        if (_amount > remainingTokens) {
+            _amount = remainingTokens;
+        }
+
+        // Register tokens to the receiver
+        balances[_to] = balances[_to].add(_amount);
+
+        // Update total amount of tokens issued
+        airDropTokenIssuedTotal = airDropTokenIssuedTotal.add(_amount);
+
+        // Lock the receiver
+        locked[_to] = true;
+
+        Transfer(address(0x0), _to, _amount);
+    }
+
+    function rewardBounty(address _to, uint _amount) onlyOwner {
+
+        require(bountyTokenIssuedTotal < TOKEN_SUPPLY_BOUNTY_LIMIT);
+
+        uint remainingTokens = TOKEN_SUPPLY_BOUNTY_LIMIT.sub(bountyTokenIssuedTotal);
+        if (_amount > remainingTokens) {
+            _amount = remainingTokens;
+        }
+
+        // Register tokens to the receiver
+        balances[_to] = balances[_to].add(_amount);
+
+        // Update total amount of tokens issued
+        bountyTokenIssuedTotal = bountyTokenIssuedTotal.add(_amount);
+
+        // Lock the receiver
+        locked[_to] = true;
+
+        Transfer(address(0x0), _to, _amount);
+    }
+
+    function transferSeedToken(address _to, uint _amount) onlyOwner {
+
+        require(seedTokenIssuedTotal < TOKEN_SUPPLY_SEED_LIMIT);
+
+        uint remainingTokens = TOKEN_SUPPLY_SEED_LIMIT.sub(seedTokenIssuedTotal);
+        require (_amount <= remainingTokens);
+
+        // Register tokens to the receiver
+        balances[_to] = balances[_to].add(_amount);
+
+        // Update total amount of tokens issued
+        seedTokenIssuedTotal = seedTokenIssuedTotal.add(_amount);
+
+        // Do not lock the seed
+
+        Transfer(address(0x0), _to, _amount);
+    }
+
+    function transferSeedTokenMany(address[] addrList, uint[] amountList) onlyOwner {
+
+        require(addrList.length == amountList.length);
+
+        for (uint i = 0; i < addrList.length; i++) {
+
+            transferSeedToken(addrList[i], amountList[i]);
+        }
+    }
+
+    function transferPreSaleToken(address _to, uint _amount) onlyOwner {
+
+        require(presaleTokenIssuedTotal < TOKEN_SUPPLY_PRESALE_LIMIT);
+
+        uint remainingTokens = TOKEN_SUPPLY_PRESALE_LIMIT.sub(presaleTokenIssuedTotal);
+        require (_amount <= remainingTokens);
+
+        // Register tokens to the receiver
+        balances[_to] = balances[_to].add(_amount);
+
+        // Update total amount of tokens issued
+        presaleTokenIssuedTotal = presaleTokenIssuedTotal.add(_amount);
+
+        // Do not lock the presale
+
+        Transfer(address(0x0), _to, _amount);
+    }
+
+    function transferPreSaleTokenMany(address[] addrList, uint[] amountList) onlyOwner {
+
+        require(addrList.length == amountList.length);
+
+        for (uint i = 0; i < addrList.length; i++) {
+
+            transferPreSaleToken(addrList[i], amountList[i]);
+        }
+    }
+}
+
+contract MainSale is WhitelistedCrowdsale, usingOraclize {
+    
+    using SafeMath for uint;
+
+    ShareToken public shrToken;
+
+    // Any token amount must be multiplied by this const to reflect decimals
+    uint constant E2 = 10**2;
 
     bool public isIcoRunning = true;
     bool public isUpdateRateRunning = true;
@@ -183,31 +364,20 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
     uint public tokenPriceInCent = 2; // cent or $0.02
     uint public ethUsdRateInCent = 0;// cent
 
-    uint constant TOKEN_SUPPLY_TOTAL_LIMIT    = 1400000000 * E2; // (1.4 billion)
     uint public constant TOKEN_SUPPLY_MAINSALE_LIMIT = 1000000000 * E2; // 1,000,000,000 tokens (1 billion)
-    uint public constant TOKEN_SUPPLY_AIRDROP_LIMIT  = 6666666667; // 66,666,666.67 tokens (0.066 billion)
-    uint public constant TOKEN_SUPPLY_BOUNTY_LIMIT   = 33333333333; // 333,333,333.33 tokens (0.333 billion)
-
-    uint public airDropTokenIssuedTotal    = 0;
-    uint public bountyTokenIssuedTotal     = 0;
 
     uint public constant ETH_USD_UPDATE_PERIOD = 86400; // 24 * 60 * 60 (for every 24 hours) 
 
-    mapping(address => uint) airDropTokenIssuedList;
-    mapping(address => uint) bountyTokenIssuedList;
-
-    mapping(address => bool) locked;
-
+    // Must pre-deploy the ShareToken contract to get its address
     // Must supply the ETH/USD rate (in cent) upon token creation
-    function ShareToken(uint _ethUsdRateInCent) payable {
+    function MainSale(uint _ethUsdRateInCent, address _tokenAddress) payable {
 
-        uint tokenSupplyLimit = TOKEN_SUPPLY_MAINSALE_LIMIT + TOKEN_SUPPLY_AIRDROP_LIMIT + TOKEN_SUPPLY_BOUNTY_LIMIT; 
-        require( tokenSupplyLimit == TOKEN_SUPPLY_TOTAL_LIMIT );
-        
         require(_ethUsdRateInCent > 0);
+        require( _tokenAddress != address(0x0) );
 
-        totalTokenIssued = 0;
         ethUsdRateInCent = _ethUsdRateInCent;
+
+        shrToken = ShareToken(_tokenAddress);
 
         updateRate();
     }
@@ -241,10 +411,12 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
         // Calc the token amount
         tokens = transferredAmount.div(tokenPriceInCent) * E2;
 
-        // If the allocated tokens exceed the limit, must refund to user
-        if (totalTokenIssued.add(tokens) > TOKEN_SUPPLY_MAINSALE_LIMIT) {
+        uint totalIssuedTokens = shrToken.totalSupply();
 
-            uint tokensAvailable = TOKEN_SUPPLY_MAINSALE_LIMIT - totalTokenIssued;
+        // If the allocated tokens exceed the limit, must refund to user
+        if (totalIssuedTokens.add(tokens) > TOKEN_SUPPLY_MAINSALE_LIMIT) {
+
+            uint tokensAvailable = TOKEN_SUPPLY_MAINSALE_LIMIT - totalIssuedTokens;
             uint tokensToRefund = tokens - tokensAvailable;
             uint ethToRefundInWei = tokensToRefund.div(tokenPriceInCent).div(ethUsdRateInCent).mul(10**18);
             
@@ -258,16 +430,7 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
             isIcoRunning = false;
         }
 
-        // Allocate tokens to the buyer
-        balances[msg.sender] = balances[msg.sender].add(tokens);
-
-        // Update total amount of tokens issued
-        totalTokenIssued = totalTokenIssued.add(tokens);
-
-        // Lock the buyer
-        locked[msg.sender] = true;
-
-        Transfer(address(0x0), msg.sender, tokens);
+        shrToken.sell(msg.sender, tokens);
     }
 
     function withdrawToOwner() payable {
@@ -280,27 +443,6 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
 
         require(msg.sender == owner);
         _to.transfer(this.balance);
-    }
-
-    function unLock(address _participant) onlyOwner {
-
-        locked[_participant] = false;
-    }
-
-    function unLockMultiple(address[] _participants) onlyOwner {
-
-        for (uint i = 0; i < _participants.length; i++) {
-            locked[_participants[i]] = false;
-        }
-    }
-
-    function unLockAllWhitelist() onlyOwner {
-
-        require (isIcoRunning == false);
-
-        for (uint i = 0; i < whitelistAddresses.length; i++) {
-            locked[whitelistAddresses[i]] = false;
-        }
     }
 
     function setEthUsdRateInCent(uint _ethUsdRateInCent) onlyOwner {
@@ -324,71 +466,9 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
         isIcoRunning = true;
     }
 
-    function rewardAirdrop(address _to, uint _amount) onlyOwner {
-
-        require(isIcoRunning == true);
-        require(airDropTokenIssuedTotal < TOKEN_SUPPLY_AIRDROP_LIMIT);
-
-        uint remainingTokens = TOKEN_SUPPLY_AIRDROP_LIMIT.sub(airDropTokenIssuedTotal);
-        if (_amount > remainingTokens) {
-            _amount = remainingTokens;
-        }
-
-        // Allocate tokens to the receiver
-        balances[_to] = balances[_to].add(_amount);
-
-        // Update total amount of tokens issued
-        airDropTokenIssuedTotal = airDropTokenIssuedTotal.add(_amount);
-
-        // Lock the receiver
-        locked[_to] = true;
-
-        Transfer(address(0x0), _to, _amount);
-    }
-
-    function rewardBounty(address _to, uint _amount) onlyOwner {
-
-        require(isIcoRunning == true);
-        require(bountyTokenIssuedTotal < TOKEN_SUPPLY_BOUNTY_LIMIT);
-
-        uint remainingTokens = TOKEN_SUPPLY_BOUNTY_LIMIT.sub(bountyTokenIssuedTotal);
-        if (_amount > remainingTokens) {
-            _amount = remainingTokens;
-        }
-
-        // Allocate tokens to the receiver
-        balances[_to] = balances[_to].add(_amount);
-
-        // Update total amount of tokens issued
-        bountyTokenIssuedTotal = bountyTokenIssuedTotal.add(_amount);
-
-        // Lock the receiver
-        locked[_to] = true;
-
-        Transfer(address(0x0), _to, _amount);
-    }
-
-    /* Override "transfer" (ERC20) */
-    function transfer(address _to, uint _amount) returns (bool success) {
-
-        require( locked[msg.sender] == false );    
-        require( locked[_to] == false );
-        
-        return super.transfer(_to, _amount);
-    }
-
-    /* Override "transferFrom" (ERC20) */
-    function transferFrom(address _from, address _to, uint _amount) returns (bool success) {
-        
-        require( locked[_from] == false );
-        require( locked[_to] == false );
-
-        return super.transferFrom(_from, _to, _amount);
-    }
-
     function remainingTokensForSale() constant returns (uint) {
         
-        return TOKEN_SUPPLY_MAINSALE_LIMIT.sub(totalTokenIssued);
+        return TOKEN_SUPPLY_MAINSALE_LIMIT.sub(shrToken.totalSupply());
     }
 
     function __callback(bytes32 myid, string result) {
@@ -406,6 +486,8 @@ contract ShareToken is ERC20Token, WhitelistedCrowdsale, usingOraclize {
 
     function updateRate() payable {
         
+        require(msg.sender == owner);
+
         if (oraclize_getPrice("URL") <= this.balance) {
             // Get rate with delay
             oraclize_query(ETH_USD_UPDATE_PERIOD, "URL", "json(https://api.gdax.com/products/ETH-USD/ticker).price");
