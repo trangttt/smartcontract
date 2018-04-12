78084801e663b27de7a0181903ae19093eab1193
Trang Tran <thuytranga1@yahoo.com>
2018-04-12 22:24:13 +0700

    merge with trungtt

--- app/global.js
+++ app/global.js
@@ -1,16 +1,23 @@
+const ShareToken = artifacts.require('ShareToken');
+
 var Global = function () {}
 
 Global.globalize = function () {
 
     global.OWNER_ADDR = "0x175FeA8857f7581B971C5a41F27Ea4BB43356298";
     global.PRIV_KEY = "c548e41cdc3f372995820c3d52fc470b2b380e1bce2a86be017b50e2f2fe82ee";
-    global.SHARE_TOKEN_CONTRACT_ADDR = "0x30bbcd8f3b12a2b75677fb434e33ea2fe42b6199";
+    global.SHARE_TOKEN_CONTRACT_ADDR = "0x3472eca8d2a781dbe18378558d82eb286e5d3534";
 
     // Reference to the deployed contract via its address
-    var ShrToken = artifacts.require('./ShareToken.sol');
-    var ShrTokenContractABI = require('./../build/contracts/ShareToken.json').abi;
-    var ShrTokenContract = web3.eth.contract(ShrTokenContractABI);
-    global.ShrTokenObj = ShrTokenContract.at(SHARE_TOKEN_CONTRACT_ADDR);
+    //var ShrToken = artifacts.require('./ShareToken.sol');
+    //var ShrTokenContractABI = require('./../build/contracts/ShareToken.json').abi;
+    //var ShrTokenContract = web3.eth.contract(ShrTokenContractABI);
+    //global.ShrTokenObj = ShrTokenContract.at(SHARE_TOKEN_CONTRACT_ADDR);
+    global.ShrTokenObj;
+    ShareToken.deployed().then(function(instance){
+        global.ShrTokenObj = instance;
+        console.log("ShareToken: ", global.ShrTokenObj.address);
+    });
 
 }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,8 @@
 var ShareToken = artifacts.require("./ShareToken.sol");
 var MainSale = artifacts.require("./MainSale.sol");
-var WhiteListManager = artifacts.require("./WhitelistManager");
 
 module.exports = function(deployer) {
-  var stAddr;
-  deployer.deploy(WhiteListManager).then(function(){
-    return deployer.deploy(ShareToken).then(function(){
-      console.log('SHARE TOKEN', ShareToken.address);
+    deployer.deploy(ShareToken).then(function(){
       return deployer.deploy(MainSale, 40000, ShareToken.address);
     });
-  }); 
 };
